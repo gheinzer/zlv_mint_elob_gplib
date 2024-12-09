826c15ff4eeb361cf4e0473ddab79212416e809f
Gabriel Heinzer <dev@gabrielheinzer.ch>
2024-12-09 21:31:28 +0100

    Added silent-error capability

--- src/elob/util/error.c
+++ src/elob/util/error.c
@@ -8,6 +8,7 @@
 #include <elob/util/terminal.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
 jmp_buf _error_h_globalJmpBuffer;
 
@@ -23,16 +24,6 @@ void error_init() {
 }
 
 void _error_h_throw(unsigned int errorCode, const char* errorName) {
-	// Print the error name to the terminal
-	terminal_setStyle(TERMINAL_STYLE_RESET);
-	printf("\r\n\r\n");
-	terminal_setColors(TERMINAL_CLR_BLACK, TERMINAL_CLR_RED);
-	printf(" ");
-	printf(errorName);
-	printf(" ");
-	terminal_setStyle(TERMINAL_STYLE_RESET);
-	printf("\r\n");
-
 	// Store the error name and message in the global variables
 	_error_h_currentErrorName = errorName;
 	_error_h_currentErrorMessage = 0; // Null pointer as no error message was thrown
@@ -42,19 +33,6 @@ void _error_h_throw(unsigned int errorCode, const char* errorName) {
 }
 
 void _error_h_throwWithMessage(unsigned int errorCode, const char* errorName, const char* errorMessage) {
-	// Print the error name and message to the terminal
-	terminal_setStyle(TERMINAL_STYLE_RESET);
-	printf("\r\n\r\n");
-	terminal_setColors(TERMINAL_CLR_BLACK, TERMINAL_CLR_RED);
-	printf(" ");
-	printf(errorName);
-	printf(" ");
-	terminal_setStyle(TERMINAL_STYLE_RESET);
-	terminal_setStyle(TERMINAL_STYLE_BOLD);
-	printf(" ");
-	printf("%s\r\n", errorMessage);
-	terminal_setStyle(TERMINAL_STYLE_RESET);
-
 	// Store the error name and message in the global variables
 	_error_h_currentErrorName = errorName;
 	_error_h_currentErrorMessage = errorMessage;
@@ -73,10 +51,20 @@ void _error_h_uncaughtErrorHandler() {
 	CLEARBIT(PORTB, 6);
 	CLEARBIT(PORTB, 7);
 	
+	// Print the error information to stderr
+	terminal_setStyle(TERMINAL_STYLE_RESET);
+	fprintf(stderr, "\r\n");
+	terminal_setColors(TERMINAL_CLR_BLACK, TERMINAL_CLR_RED);
+	fprintf(stderr, " %s ", _error_h_currentErrorName);
+	terminal_setStyle(TERMINAL_STYLE_RESET);
+	terminal_setStyle(TERMINAL_STYLE_BOLD);
+	fprintf(stderr, " %s\r\n", _error_h_currentErrorMessage);
+	terminal_setStyle(TERMINAL_STYLE_RESET);
+	
 	// Inform the user that the error was not caught
 	terminal_setStyle(TERMINAL_STYLE_DIM);
-	printf("%s\r\n", "The above error was not caught.");
-	printf("%s\r\n", "Reset the board to continue operation.");
+	fprintf(stderr, "%s\r\n", "The above error was not caught.");
+	fprintf(stderr, "%s\r\n", "Reset the board to continue operation.");
 	
 	// Enter an infinite loop
 	while(1);
